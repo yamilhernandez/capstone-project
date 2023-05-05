9294fe5c813d1a79af7b9419ae186f1694696f6e
crisvalsan <crisvalentin891@gmail.com>
2023-05-04 20:59:21 -0400

    url fix

--- CompraSometida/urls.py
+++ CompraSometida/urls.py
@@ -2,14 +2,13 @@ from django.urls import path
 from CompraSometida import views
 from django.contrib import admin
 
-from .views import (submitted, sometidaView, detailedView,submitAll, deleteAll)
+from .views import (submitted, sometidaView,submitAll, deleteAll)
 
 #app_name = 'Compras'
 urlpatterns = [
 
     path('sometido', views.submitted, name='sometido'),
     path('sometida', views.sometidaView, name='sometida'),
-    path('detallado', views.detailedView, name='detallado'),# borrar
     path('submitAll', views.submitAll, name='submitAll'),
     path('deleteAll', views.deleteAll, name='deleteAll') 
     #los htmls intente ponerles la palabra sometido para identidficar.
